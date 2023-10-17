4062ca1954cb0e14a98ba65d4a8a1f5910d30acf
rodrigosuelli <rodrigosuelli@gmail.com>
2023-10-16 22:45:18 -0300

    GenerateStaticParams for all products

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -5,10 +5,14 @@ import { notFound } from 'next/navigation';
 import Image from 'next/image';
 import styles from './produto.module.css';
 
-export async function generateStaticParams() {
+async function fetchSlugsByPage({ page }) {
   const query = qs.stringify(
     {
       fields: ['slug'],
+      pagination: {
+        page,
+        pageSize: 250,
+      },
     },
     {
       encodeValuesOnly: true, // prettify URL
@@ -17,19 +21,43 @@ export async function generateStaticParams() {
 
   const url = `${process.env.STRAPI_API_URL}/produtos?${query}`;
 
-  const res = await fetch(url, {
+  const response = await fetch(url, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
     },
-  });
+  }).then((res) => res.json());
 
-  const produtos = await res.json();
+  return response;
+}
 
-  return produtos.data.map((produto) => ({
-    slug: produto.attributes.slug,
-  }));
+export async function generateStaticParams() {
+  const getAllDataPromiseArray = [];
+
+  const initialRes = await fetchSlugsByPage({ page: 1 });
+
+  const { page, pageCount } = initialRes.meta.pagination;
+
+  if (page < pageCount) {
+    for (let i = page + 1; i <= pageCount; i++) {
+      const res = fetchSlugsByPage({ page: i });
+      getAllDataPromiseArray.push(res);
+    }
+  }
+
+  const allResponses = await Promise.all(getAllDataPromiseArray);
+  allResponses.push(initialRes);
+
+  const staticParamsArrays = allResponses.map((req) =>
+    req.data.map((produto) => ({
+      slug: produto.attributes.slug,
+    }))
+  );
+
+  const staticParams = staticParamsArrays.flat();
+
+  return staticParams;
 }
 
 async function getData(slug) {
