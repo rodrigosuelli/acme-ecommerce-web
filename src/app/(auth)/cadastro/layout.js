9a6dbf203ebb994392e63e2321ce168b9bfb2d79
rodrigosuelli <rodrigosuelli@gmail.com>
2023-10-18 19:19:51 -0300

    Add CadastroLayout

---
+++ src/app/(auth)/cadastro/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Acme - Cadastro',
+  description: 'Tela de cadastro da Acme',
+};
+
+export default function CadastroLayout({ children }) {
+  return children;
+}
