021da9ec0709e438107d604f27105e9614f56f7b
rodrigosuelli <rodrigosuelli@gmail.com>
2023-10-18 20:16:48 -0300

    Adicionar tela de cadastro base

---
+++ src/app/(auth)/cadastro/page.js
@@ -0,0 +1,112 @@
+'use client';
+
+import { CgSpinner } from 'react-icons/cg';
+import { useState } from 'react';
+import Link from 'next/link';
+import { useUser } from '@/contexts/userContext';
+import withAuthRoute from '@/hoc/withAuthRoute';
+
+function Cadastro() {
+  const [isSendingForm, setIsSendingForm] = useState(false);
+
+  const { register } = useUser();
+
+  async function handleRegister(e) {
+    e.preventDefault();
+    setIsSendingForm(true);
+
+    const form = e.target;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+
+    const { name, email, password, confirm_password, data_nasc, celular } =
+      formJson;
+
+    await register({
+      name,
+      email,
+      password,
+      confirm_password,
+      data_nasc,
+      celular,
+    });
+
+    setIsSendingForm(false);
+  }
+
+  return (
+    <form onSubmit={handleRegister} className="authForm">
+      <h1>Deseja ficar por dentro?</h1>
+      <p>Crie uma conta para poder ter acesso completo ao site.</p>
+      <label htmlFor="name">Nome Completo:</label>
+      <input
+        required
+        minLength={3}
+        autoComplete="name"
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Insira seu nome completo..."
+      />
+      <label htmlFor="email">Email:</label>
+      <input
+        required
+        minLength={6}
+        autoComplete="email"
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Insira seu email..."
+      />
+      <label htmlFor="password">Senha:</label>
+      <input
+        required
+        minLength={6}
+        autoComplete="new-password"
+        placeholder="Senha"
+        type="password"
+        name="password"
+        id="password"
+      />
+      <label htmlFor="confirm_password">Confirmar Senha:</label>
+      <input
+        required
+        minLength={6}
+        autoComplete="new-password"
+        placeholder="Digite novamente a senha..."
+        type="password"
+        name="confirm_password"
+        id="confirm_password"
+      />
+      <label htmlFor="data_nasc">Data de nascimento:</label>
+      <input
+        required
+        autoComplete="bday"
+        type="date"
+        name="data_nasc"
+        id="data_nasc"
+      />
+      <label htmlFor="celular">N° de celular:</label>
+      <input
+        required
+        autoComplete="tel-national"
+        type="text"
+        name="celular"
+        id="celular"
+        placeholder="Telefone celular..."
+      />
+
+      <button disabled={isSendingForm} className="btnEnter" type="submit">
+        {isSendingForm ? <CgSpinner size={28} /> : 'Criar Conta'}
+      </button>
+      <p className="paragraphWithLink">
+        Já possui uma conta?&nbsp;
+        <Link href="/cadastro" className="textLink">
+          Efetue o Login
+        </Link>
+      </p>
+    </form>
+  );
+}
+
+export default withAuthRoute(Cadastro);
