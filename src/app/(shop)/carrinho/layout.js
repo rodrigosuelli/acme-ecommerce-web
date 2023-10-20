bcdc434b0b862bfc04d787fc8d20d217b574d885
rodrigosuelli <rodrigosuelli@gmail.com>
2023-10-20 20:18:08 -0300

    Add carrinho base

---
+++ src/app/(shop)/carrinho/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Acme - Carrinho',
+  description: 'Tela de carrinho da Acme',
+};
+
+export default function CarrinhoLayout({ children }) {
+  return children;
+}
---
+++ src/app/(shop)/carrinho/page.js
@@ -0,0 +1,11 @@
+import styles from './carrinho.module.css';
+
+function Carrinho() {
+  return (
+    <div className={styles.carrinhoContainer}>
+      <h1>Carrinho</h1>
+    </div>
+  );
+}
+
+export default Carrinho;
