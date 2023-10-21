0b55ffc308ac8725badc7d12cf1515bcc80346ac
rodrigosuelli <rodrigosuelli@gmail.com>
2023-10-21 19:55:15 -0300

    Add cart methods

--- src/app/contexts/cartContext.js
+++ src/app/contexts/cartContext.js
@@ -1,5 +1,6 @@
 import { createContext, useContext, useCallback, useMemo } from 'react';
 
+import { toast } from 'react-toastify';
 import useLocalStorageCustom from '../hooks/useLocalStorageCustom';
 
 export const CartContext = createContext();
@@ -17,7 +18,80 @@ export default function CartContextComp({ children }) {
       if (!cart?.length) {
         setCart([{ id: itemId, qtd: 1 }]);
       } else {
-        setCart([...cart, { id: itemId, qtd: 1 }]);
+        const itemExists = cart.some((item) => item.id === itemId);
+
+        if (!itemExists) {
+          setCart([...cart, { id: itemId, qtd: 1 }]);
+        } else {
+          toast.error('Oops, este produto já foi adicionado no seu carrinho.', {
+            position: 'bottom-right',
+          });
+        }
+      }
+    },
+    [cart, setCart]
+  );
+
+  const changeItemQuantity = useCallback(
+    (itemId, newQtd) => {
+      if (!cart) {
+        setCart(null);
+      }
+
+      if (!cart?.length) {
+        toast.error(
+          'Oops, não é possível realizar esta ação pois seu carrinho está vazio.',
+          {
+            position: 'bottom-right',
+          }
+        );
+        return;
+      }
+
+      const itemExists = cart.some((item) => item.id === itemId && item.qtd);
+
+      if (itemExists) {
+        setCart(
+          cart.map((item) => {
+            if (item.id === itemId) {
+              return { ...item, qtd: newQtd };
+            }
+            return item;
+          })
+        );
+      } else {
+        toast.error('Oops, este produto não está no seu carrinho.', {
+          position: 'bottom-right',
+        });
+      }
+    },
+    [cart, setCart]
+  );
+
+  const removeItemFromCart = useCallback(
+    (itemId) => {
+      if (!cart) {
+        setCart(null);
+      }
+
+      if (!cart?.length) {
+        toast.error(
+          'Oops, não é possível realizar esta ação pois seu carrinho está vazio.',
+          {
+            position: 'bottom-right',
+          }
+        );
+        return;
+      }
+
+      const itemExists = cart.some((item) => item.id === itemId && item.qtd);
+
+      if (itemExists) {
+        setCart(cart.filter((item) => item.id !== itemId));
+      } else {
+        toast.error('Oops, este produto não está no seu carrinho.', {
+          position: 'bottom-right',
+        });
       }
     },
     [cart, setCart]
@@ -31,9 +105,11 @@ export default function CartContextComp({ children }) {
     () => ({
       cart,
       addItemToCart,
+      changeItemQuantity,
+      removeItemFromCart,
       clearCart,
     }),
-    [cart, addItemToCart, clearCart]
+    [cart, addItemToCart, changeItemQuantity, removeItemFromCart, clearCart]
   );
 
   return (
