dc81139e4437b4390e9903c2d09a333eb34099b2
rodrigosuelli <rodrigosuelli@gmail.com>
2023-10-22 19:41:07 -0300

    teste2

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -4,30 +4,37 @@ import { headers } from 'next/headers';
 
 // Revalidate webhook to `your-website.com/api/revalidate
 export async function POST(request) {
-  const headersList = headers();
+  try {
+    const headersList = headers();
 
-  // Extract secret from header: 'Bearer jqklmaosdpakm' => 'jqklmaosdpakm'
-  const authHeader = headersList.get('Authorization');
-  const secret = authHeader && authHeader.split(' ')[1];
+    // Extract secret from header: 'Bearer jqklmaosdpakm' => 'jqklmaosdpakm'
+    const authHeader = headersList.get('Authorization');
+    const secret = authHeader && authHeader.split(' ')[1];
 
-  const data = await request.json();
+    const data = await request.json();
 
-  console.log(data);
-  console.log('==========================');
+    console.log(data);
+    console.log('==========================');
 
-  const { model } = data;
+    const { model } = data;
 
-  if (model !== 'produto') {
-    return Response.json({ message: 'Model is not produto' }, { status: 400 });
+    if (model !== 'produto') {
+      return Response.json(
+        { message: 'Model is not produto' },
+        { status: 400 }
+      );
+    }
+
+    if (secret !== process.env.NEXT_REVALIDATION_SECRET) {
+      return Response.json({ message: 'Invalid secret' }, { status: 401 });
+    }
+
+    // revalidateTag('product');
+
+    console.log('revalidated with success');
+
+    return Response.json({ revalidated: true, now: Date.now() });
+  } catch (error) {
+    console.log(error);
   }
-
-  if (secret !== process.env.NEXT_REVALIDATION_SECRET) {
-    return Response.json({ message: 'Invalid secret' }, { status: 401 });
-  }
-
-  // revalidateTag('product');
-
-  console.log('revalidated with success');
-
-  return Response.json({ revalidated: true, now: Date.now() });
 }
