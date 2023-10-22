8f919a7810b5506d148fa5866dfea677cfd631f2
rodrigosuelli <rodrigosuelli@gmail.com>
2023-10-22 19:49:28 -0300

    test4

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -4,37 +4,31 @@ import { headers } from 'next/headers';
 
 // Revalidate webhook to `your-website.com/api/revalidate
 export async function POST(request) {
-  try {
-    const headersList = headers();
+  const headersList = headers();
 
-    // Extract secret from header: 'Bearer jqklmaosdpakm' => 'jqklmaosdpakm'
-    const authHeader = headersList.get('Authorization');
-    const secret = authHeader && authHeader.split(' ')[1];
+  // Extract secret from header: 'Bearer jqklmaosdpakm' => 'jqklmaosdpakm'
+  const authHeader = headersList.get('Authorization');
+  const secret = authHeader && authHeader.split(' ')[1];
+  console.log(data.teste.taste);
 
-    const data = await request.json();
+  const data = await request.json();
 
-    console.log(data.teste.taste);
-    console.log('==========================');
+  console.log(data.teste.taste);
+  console.log('==========================');
 
-    const { model } = data;
+  const { model } = data;
 
-    if (model !== 'produto') {
-      return Response.json(
-        { message: 'Model is not produto' },
-        { status: 400 }
-      );
-    }
-
-    if (secret !== process.env.NEXT_REVALIDATION_SECRET) {
-      return Response.json({ message: 'Invalid secret' }, { status: 401 });
-    }
-
-    // revalidateTag('product');
-
-    console.log('revalidated with success');
-
-    return Response.json({ revalidated: true, now: Date.now() });
-  } catch (error) {
-    console.log(error);
+  if (model !== 'produto') {
+    return Response.json({ message: 'Model is not produto' }, { status: 400 });
   }
+
+  if (secret !== process.env.NEXT_REVALIDATION_SECRET) {
+    return Response.json({ message: 'Invalid secret' }, { status: 401 });
+  }
+
+  // revalidateTag('product');
+
+  console.log('revalidated with success');
+
+  return Response.json({ revalidated: true, now: Date.now() });
 }
