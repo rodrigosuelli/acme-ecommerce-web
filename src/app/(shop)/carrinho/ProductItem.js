c7efdb50c9868093b573bf2c6af3603831e498ea
rodrigosuelli <rodrigosuelli@gmail.com>
2023-10-22 11:54:50 -0300

    Alterações na pgina de carrinho

---
+++ src/app/(shop)/carrinho/ProductItem.js
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+import { useCart } from '../../contexts/cartContext';
+import styles from './carrinho.module.css';
+
+function ProductItem({ produtoData }) {
+  const { cart } = useCart();
+
+  const { id } = produtoData;
+  const cartItem = cart.find((item) => item.id === id);
+
+  const { titulo, preco_real, imagens } = produtoData.attributes;
+  const preco = preco_real.toFixed(2).toString().replace('.', ',');
+  const precoEm10X = (preco_real / 10).toFixed(2).toString().replace('.', ',');
+  const imgThumbUrl = imagens?.data[0]?.attributes.formats?.thumbnail.url;
+
+  return (
+    <div key={id} className={styles.produtoItemContainer}>
+      <Image
+        priority={true}
+        src={imgThumbUrl}
+        alt="imagem miniatura do produto"
+        width={100}
+        height={100}
+      />
+      <div className={styles.produtoInfo}>
+        <h2>{titulo}</h2>
+        <h3>{preco}</h3>
+        <p>10x sem juros de R${precoEm10X}</p>
+        <p>Quantidade: {cartItem.qtd}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProductItem;
--- src/app/(shop)/carrinho/page.js
+++ src/app/(shop)/carrinho/page.js
@@ -1,7 +1,23 @@
+'use client';
+
+import { useCart } from '@/contexts/cartContext';
 import InputCEP from '../../components/InputCEP/InputCEP';
 import styles from './carrinho.module.css';
+import ProductList from './ProductList';
 
 function Carrinho() {
+  const { cart } = useCart();
+
+  let productIdArray = [];
+
+  const isCartValid = cart && cart?.length;
+
+  if (isCartValid) {
+    productIdArray = cart.map((item) => item.id);
+  }
+
+  const shouldRenderProductList = isCartValid && productIdArray.length;
+
   return (
     <div className={`shopPage ${styles.carrinhoContainer}`}>
       <h1>Adicionar CEP</h1>
@@ -9,6 +25,9 @@ function Carrinho() {
       <InputCEP />
       <h1 className={styles.titleMgTop}>Produtos Selecionados</h1>
       <div className={styles.marker}></div>
+      {shouldRenderProductList && (
+        <ProductList productIdArray={productIdArray} />
+      )}
     </div>
   );
 }
