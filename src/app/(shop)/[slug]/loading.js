e5225f37f3ddcc04a28b4c736c669e61276edda5
rodrigosuelli <rodrigosuelli@gmail.com>
2023-10-23 12:13:55 -0300

    Add spinner

---
+++ src/app/(shop)/[slug]/loading.js
@@ -0,0 +1,6 @@
+import { CgSpinner } from 'react-icons/cg';
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <CgSpinner className="spinner" size={36} />;
+}
