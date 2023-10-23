848456be7d152e9778e75c3c6c40cd95c744f889
rodrigosuelli <rodrigosuelli@gmail.com>
2023-10-23 13:41:52 -0300

    Fix loading produto

--- src/app/(shop)/[slug]/loading.js
+++ src/app/(shop)/[slug]/loading.js
@@ -2,5 +2,9 @@ import { CgSpinner } from 'react-icons/cg';
 
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return <CgSpinner className="spinner" size={36} />;
+  return (
+    <div className="loadingProdutoPage">
+      <CgSpinner className="spinner" size={36} />
+    </div>
+  );
 }
