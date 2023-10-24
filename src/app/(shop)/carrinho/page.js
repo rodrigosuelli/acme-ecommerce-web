44416688cf1c8ddc2add0e94c14ea41e10fefe13
rodrigosuelli <rodrigosuelli@gmail.com>
2023-10-23 22:44:21 -0300

    comment

--- src/app/(shop)/carrinho/page.js
+++ src/app/(shop)/carrinho/page.js
@@ -12,7 +12,8 @@ import api from '../../services/api';
 const fetcher = (url) => api.get(url).then((res) => res.data);
 
 function Carrinho() {
-  const { cart, setCart } = useCart();
+  // const { cart, setCart } = useCart();
+  const { cart } = useCart();
 
   const [savedData, setSavedData] = useState(null);
 
