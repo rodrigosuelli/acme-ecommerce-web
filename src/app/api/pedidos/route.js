ad8406b217dfbfcd3d8db22d9cd78861f7a4dd29
rodrigosuelli <rodrigosuelli@gmail.com>
2023-10-28 10:27:40 -0300

    Add get pedidos route

--- src/app/api/pedidos/route.js
+++ src/app/api/pedidos/route.js
@@ -34,3 +34,28 @@ export async function POST(request) {
     statusText: res.statusText,
   });
 }
+
+export async function GET(request) {
+  const url = `${process.env.STRAPI_API_URL}/pedidos`;
+
+  const headersList = headers();
+
+  // Extract token from header: 'Bearer jqklmaosdpakm' => 'jqklmaosdpakm'
+  const authHeader = headersList.get('Authorization');
+  const userJwtToken = authHeader && authHeader.split(' ')[1];
+
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${userJwtToken}`,
+    },
+  });
+
+  const data = await res.json();
+
+  return NextResponse.json(data, {
+    status: res.status,
+    statusText: res.statusText,
+  });
+}
