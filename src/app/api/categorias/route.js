e7f477ae31bf87d5a1a25d4f044f1dd513fb2738
rodrigosuelli <rodrigosuelli@gmail.com>
2023-10-29 12:49:08 -0300

    Add categorias route

---
+++ src/app/api/categorias/route.js
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(request) {
+  const query = request.nextUrl.search;
+
+  const url = `${process.env.STRAPI_API_URL}/categorias${query}`;
+
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+    },
+  });
+
+  const data = await res.json();
+
+  return NextResponse.json(data, {
+    status: res.status,
+    statusText: res.statusText,
+  });
+}
