97d807e7b89d2d9446897fc7064066454ab27656
Rodrigo Suélli <rodrigosuelli@gmail.com>
2023-10-30 14:57:00 +0000

    Adicionar paginacao server side na tela categoria

--- src/app/(shop)/[categoria]/page.js
+++ src/app/(shop)/[categoria]/page.js
@@ -2,8 +2,9 @@ import qs from 'qs';
 import { notFound } from 'next/navigation';
 import styles from './categoria.module.css';
 import ProdutoCategoria from './ProdutoCategoria';
+import isNumeric from '../../utils/isNumericString';
 
-async function getData(slug) {
+async function getCategoria(slug) {
   const query = qs.stringify(
     {
       filters: {
@@ -12,20 +13,6 @@ async function getData(slug) {
         },
       },
       fields: ['id', 'titulo'],
-      populate: {
-        produtos: {
-          fields: [
-            'id',
-            'titulo',
-            'preco_real',
-            'preco_original',
-            'avaliacao',
-            'qtd_avaliacoes',
-            'slug',
-          ],
-          populate: { imagens: { fields: ['url', 'formats'] } },
-        },
-      },
     },
     {
       encodeValuesOnly: true, // prettify URL
@@ -41,9 +28,6 @@ async function getData(slug) {
       Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
     },
     cache: 'no-store',
-    // next: {
-    //   tags: [`product:${slug}`],
-    // },
   });
 
   if (!res.ok) {
@@ -54,10 +38,59 @@ async function getData(slug) {
   return res.json();
 }
 
-async function Categoria({ params }) {
-  const { categoria: categoriaSlug } = params;
+async function getProdutos(categoriaId, page = 1) {
+  const query = qs.stringify(
+    {
+      filters: {
+        categorias: {
+          id: {
+            $eq: categoriaId,
+          },
+        },
+      },
+      populate: { imagens: { fields: ['url', 'formats'] } },
+      fields: [
+        'id',
+        'titulo',
+        'preco_real',
+        'preco_original',
+        'avaliacao',
+        'qtd_avaliacoes',
+        'slug',
+      ],
+      pagination: {
+        page,
+      },
+    },
+    {
+      encodeValuesOnly: true, // prettify URL
+    }
+  );
 
-  const data = await getData(categoriaSlug);
+  const url = `${process.env.STRAPI_API_URL}/produtos?${query}`;
+
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+    },
+    cache: 'no-store',
+  });
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+async function Categoria({ params, searchParams }) {
+  const { categoria: categoriaSlug } = params;
+  const { page } = searchParams;
+
+  const data = await getCategoria(categoriaSlug);
 
   const categoriaData = data.data[0];
 
@@ -65,8 +98,22 @@ async function Categoria({ params }) {
     notFound();
   }
 
-  const { titulo, produtos } = categoriaData.attributes;
-  const arrayProdutos = produtos.data;
+  const categoriaId = categoriaData.id;
+  const { titulo } = categoriaData.attributes;
+
+  if (!isNumeric(page)) {
+    notFound();
+  }
+
+  const dataProdutos = await getProdutos(categoriaId, page);
+
+  const { meta } = dataProdutos;
+
+  if (meta.pagination.page > meta.pagination.pageCount) {
+    notFound();
+  }
+
+  const arrayProdutos = dataProdutos.data;
 
   return (
     <div className={`shopPage ${styles.categoriaContainer}`}>
