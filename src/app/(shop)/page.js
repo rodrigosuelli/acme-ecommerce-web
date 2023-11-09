1919b0725b8a4f67e98378104eb370b8ef79ebd9
rodrigosuelli <rodrigosuelli@gmail.com>
2023-11-09 17:45:01 -0300

    Deixar luxo azul

--- src/app/(shop)/page.js
+++ src/app/(shop)/page.js
@@ -8,7 +8,9 @@ function Homepage() {
   return (
     <div className={`shopPage ${styles.homePage}`}>
       <div className={styles.homeContainer}>
-        <h1 className={styles.heroTitle}>Abrace o Luxo em Cada Detalhe!</h1>
+        <h1 className={styles.heroTitle}>
+          Abrace o <span>Luxo</span> em Cada Detalhe!
+        </h1>
         <p>
           Não espere mais! Explore agora mesmo nossa emocionante coleção de
           lançamentos e descubra o que há de mais recente em semijoias.
