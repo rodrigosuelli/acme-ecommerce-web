7fb96469810b8aec27783f89ad58e0cb65073483
rodrigosuelli <rodrigosuelli@gmail.com>
2023-11-09 19:46:34 -0300

    Alteracoes na Home

--- src/app/(shop)/page.js
+++ src/app/(shop)/page.js
@@ -20,11 +20,11 @@ function Homepage() {
         className={styles.heroImage}
         priority={true}
         src="/images/homeHeroImageMobile.png"
-        alt="imagem miniatura do produto"
+        alt="imagem garota propaganda"
         width={293}
         height={256}
       />
-      <div className={styles.lancamentos}>
+      <div className={styles.section}>
         <div className={styles.sectionTitleContainer}>
           <h2>Confira Os Nossos Lançamentos</h2>
           <div className={styles.marker} />
@@ -39,6 +39,44 @@ function Homepage() {
         >
           Ver Lançamentos
         </Link>
+      </div>
+      <div className={`${styles.section} ${styles.blue}`}>
+        <div className={styles.sectionTitleContainer}>
+          <h2>Descubra Nossa História De Mais De 20 Anos No Mundo Das Joias</h2>
+          <div className={styles.marker} />
+        </div>
+        <p>
+          Na Acme, nossa missão é encantar nossos clientes com joias atemporais
+          de qualidade excepcional. Com mais de 20 anos de experiência,
+          celebramos a importância das joias como guardiãs de momentos especiais
+          e símbolos de amor e amizade. Nossos valores de qualidade,
+          autenticidade e atendimento ao cliente exemplificam nossa dedicação em
+          proporcionar uma experiência única. Convidamos você a fazer parte da
+          nossa jornada de 20 anos, onde cada joia tem uma história e cada
+          cliente é uma parte valiosa da nossa trajetória.
+        </p>
+        <Link href="#" className={`btnEnter btnPrimary ${styles.sectionLink}`}>
+          Conheça Nosso Legado
+        </Link>
+        <Image
+          src="/images/historySectionImageMobile.png"
+          alt="imagem fachada da loja"
+          width={293}
+          height={256}
+        />
+      </div>
+
+      <div className={styles.homeContainer}>
+        <h2 className={styles.promosTitle}>
+          Aproveite Agora! Explore Nossas Promoções Irresistíveis
+        </h2>
+        <div className={styles.marker} />
+        <p className={styles.promosDesc}>
+          Nossas promoções são o convite perfeito para adicionar um toque extra
+          de brilho à sua vida. Explore as promoções e descubra oportunidades
+          incríveis de adquirir joias excepcionais e acessórios elegantes a
+          preços que irão encantar você.
+        </p>
       </div>
     </div>
   );
