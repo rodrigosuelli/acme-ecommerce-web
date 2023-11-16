0bb128e76f696f65672b75c4eaf00be7dd2530d2
rodrigosuelli <rodrigosuelli@gmail.com>
2023-11-16 00:01:30 -0300

    Add homepage media querys

--- src/app/(shop)/page.js
+++ src/app/(shop)/page.js
@@ -5,6 +5,7 @@ import 'react-multi-carousel/lib/styles.css';
 import styles from './homepage.module.css';
 
 import homeHeroImageMobile from '../../../public/images/homeHeroImageMobile.png';
+import historySectionImageDesktop from '../../../public/images/historySectionImageDesktop.png';
 import historySectionImageMobile from '../../../public/images/historySectionImageMobile.png';
 import InputEmail from '../components/Inputs/InputEmail';
 import CarouselFeedback from '../components/CarouselFeedback/CarouselFeedback';
@@ -13,71 +14,90 @@ import ProdutosCarouselSection from '../components/ProdutosCarouselSection/Produ
 function Homepage() {
   return (
     <div className={`shopPage ${styles.homePage}`}>
-      <div className={styles.homeContainer}>
+      <div className={styles.homeHeroContainer}>
         <h1 className={styles.heroTitle}>
           Abrace o <span>Luxo</span> em Cada Detalhe!
         </h1>
+        <div className={styles.marker} />
         <p>
           Não espere mais! Explore agora mesmo nossa emocionante coleção de
           lançamentos e descubra o que há de mais recente em semijoias.
         </p>
       </div>
-      <Image
-        className={styles.heroImage}
-        priority={true}
-        src={homeHeroImageMobile}
-        alt="imagem garota propaganda"
-      />
-      <div className={styles.section}>
-        <div className={styles.sectionTitleContainer}>
-          <h2>Confira Os Nossos Lançamentos</h2>
-          <div className={styles.marker} />
+      <div className={styles.homePageWrapper}>
+        <Image
+          className={styles.heroImage}
+          priority={true}
+          src={homeHeroImageMobile}
+          alt="imagem garota propaganda"
+        />
+        <div className={styles.section}>
+          <div className={styles.sectionTitleContainer}>
+            <h2>Confira Os Nossos Lançamentos</h2>
+            <div className={styles.marker} />
+          </div>
+          <p className={styles.lancamentosDesc}>
+            Descubra nossas novidades mais reluzentes! Na Acme, estamos
+            empolgados em apresentar as mais recentes adições à nossa coleção de
+            joias.
+          </p>
+          <ProdutosCarouselSection categoriaSlug="lancamentos" />
+          <Link
+            href="/lancamentos"
+            className={`btnEnter btnPrimary ${styles.sectionLink}`}
+          >
+            Ver Lançamentos
+          </Link>
         </div>
-        <p className={styles.lancamentosDesc}>
-          Descubra nossas novidades mais reluzentes! Na Acme, estamos empolgados
-          em apresentar as mais recentes adições à nossa coleção de joias.
-        </p>
-        <ProdutosCarouselSection categoriaSlug="lancamentos" />
-        <Link
-          href="/lancamentos"
-          className={`btnEnter btnPrimary ${styles.sectionLink}`}
-        >
-          Ver Lançamentos
-        </Link>
-      </div>
-      <div className={`${styles.section} ${styles.blue}`}>
-        <div className={styles.sectionTitleContainer}>
-          <h2>Descubra Nossa História De Mais De 20 Anos No Mundo Das Joias</h2>
-          <div className={styles.marker} />
+        <div className={`${styles.section} ${styles.blue}`}>
+          <div className={styles.sectionTitleContainer}>
+            <h2>
+              Descubra Nossa História De Mais De 20 Anos No Mundo Das Joias
+            </h2>
+            <div className={styles.marker} />
+          </div>
+          <p>
+            Na Acme, nossa missão é encantar nossos clientes com joias
+            atemporais de qualidade excepcional. Com mais de 20 anos de
+            experiência, celebramos a importância das joias como guardiãs de
+            momentos especiais e símbolos de amor e amizade. Nossos valores de
+            qualidade, autenticidade e atendimento ao cliente exemplificam nossa
+            dedicação em proporcionar uma experiência única. Convidamos você a
+            fazer parte da nossa jornada de 20 anos, onde cada joia tem uma
+            história e cada cliente é uma parte valiosa da nossa trajetória.
+          </p>
+          <Link
+            href="#"
+            className={`btnEnter btnPrimary ${styles.sectionLink}`}
+          >
+            Conheça Nosso Legado
+          </Link>
+          <Image
+            className={styles.historyImageMobile}
+            src={historySectionImageMobile}
+            alt="imagem fachada da loja"
+          />
+          <Image
+            className={styles.historyImageDesktop}
+            src={historySectionImageDesktop}
+            alt="imagem fachada da loja"
+          />
         </div>
-        <p>
-          Na Acme, nossa missão é encantar nossos clientes com joias atemporais
-          de qualidade excepcional. Com mais de 20 anos de experiência,
-          celebramos a importância das joias como guardiãs de momentos especiais
-          e símbolos de amor e amizade. Nossos valores de qualidade,
-          autenticidade e atendimento ao cliente exemplificam nossa dedicação em
-          proporcionar uma experiência única. Convidamos você a fazer parte da
-          nossa jornada de 20 anos, onde cada joia tem uma história e cada
-          cliente é uma parte valiosa da nossa trajetória.
-        </p>
-        <Link href="#" className={`btnEnter btnPrimary ${styles.sectionLink}`}>
-          Conheça Nosso Legado
-        </Link>
-        <Image src={historySectionImageMobile} alt="imagem fachada da loja" />
+
+        <div className={styles.homeContainer}>
+          <h2 className={styles.promosTitle}>
+            Aproveite Agora! Explore Nossas Promoções Irresistíveis
+          </h2>
+          <div className={styles.marker} />
+          <p className={styles.promosDesc}>
+            Nossas promoções são o convite perfeito para adicionar um toque
+            extra de brilho à sua vida. Explore as promoções e descubra
+            oportunidades incríveis de adquirir joias excepcionais e acessórios
+            elegantes a preços que irão encantar você.
+          </p>
+        </div>
       </div>
 
-      <div className={styles.homeContainer}>
-        <h2 className={styles.promosTitle}>
-          Aproveite Agora! Explore Nossas Promoções Irresistíveis
-        </h2>
-        <div className={styles.marker} />
-        <p className={styles.promosDesc}>
-          Nossas promoções são o convite perfeito para adicionar um toque extra
-          de brilho à sua vida. Explore as promoções e descubra oportunidades
-          incríveis de adquirir joias excepcionais e acessórios elegantes a
-          preços que irão encantar você.
-        </p>
-      </div>
       <div className={styles.feedbacks}>
         <div className={styles.sectionTitleContainer}>
           <h2>Feedbacks que nos inspiram!</h2>
@@ -85,17 +105,19 @@ function Homepage() {
         </div>
         <CarouselFeedback />
       </div>
-      <div className={styles.homeContainer}>
-        <h2 className={styles.promosTitle}>
-          Deseja receber informações sobre a loja?
-        </h2>
-        <div className={styles.marker} />
-        <p className={styles.promosDesc}>
-          Não perca nenhuma atualização emocionante e ofertas especiais da nossa
-          loja! Inscreva-se agora para receber nossas últimas novidades,
-          lançamentos e promoções diretamente na sua caixa de entrada.
-        </p>
-        <InputEmail />
+      <div className={styles.homePageWrapper}>
+        <div className={`${styles.homeContainer} ${styles.newsletter}`}>
+          <h2 className={styles.promosTitle}>
+            Deseja receber informações sobre a loja?
+          </h2>
+          <div className={styles.marker} />
+          <p className={styles.promosDesc}>
+            Não perca nenhuma atualização emocionante e ofertas especiais da
+            nossa loja! Inscreva-se agora para receber nossas últimas novidades,
+            lançamentos e promoções diretamente na sua caixa de entrada.
+          </p>
+          <InputEmail />
+        </div>
       </div>
     </div>
   );
--- src/app/components/ProdutosCarouselSection/ListaProdutosCarousel.js
+++ src/app/components/ProdutosCarouselSection/ListaProdutosCarousel.js
@@ -8,19 +8,19 @@ function ListaProdutosCarousel({ arrayProdutos }) {
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 1,
+      breakpoint: { max: 4000, min: 1299 },
+      items: 4,
     },
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 1,
+      breakpoint: { max: 1299, min: 969 },
+      items: 3,
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 1,
+      breakpoint: { max: 969, min: 759 },
+      items: 2,
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 759, min: 0 },
       items: 1,
     },
   };
