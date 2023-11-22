d52c17815c0d255ecf078cf46a5ae32ec053f1f2
rodrigosuelli <rodrigosuelli@gmail.com>
2023-11-22 20:06:56 -0300

    Auth pages redesign

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,11 +1,35 @@
-import AuthHeader from '../components/AuthHeader/AuthHeader';
+'use client';
+
+import Link from 'next/link';
 import './authPagesGlobal.css';
+import { ArrowReplyFilled } from '@fluentui/react-icons';
+import Image from 'next/image';
 
 export default function AuthPagesLayout({ children }) {
   return (
     <div className="authPageContainer">
-      <AuthHeader />
-      {children}
+      <Link href="/" className="backButtonLink">
+        <ArrowReplyFilled fontSize={26} />
+      </Link>
+      <Image
+        className="logoAuth"
+        priority={true}
+        src="/images/logoAuth.svg"
+        alt="Logo Acme"
+        width={91}
+        height={30}
+      />
+      <div className="acmeLogoBack">
+        <Image
+          className="desktopLogoAuth"
+          priority={true}
+          src="/images/logoFooter.svg"
+          alt="Logo Acme"
+          width={109}
+          height={36}
+        />
+      </div>
+      <div className="formWrapper">{children}</div>
     </div>
   );
 }
