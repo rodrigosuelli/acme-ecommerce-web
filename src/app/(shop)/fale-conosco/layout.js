49400d6a1e69e22a26023576c2a6eb9f45d45733
rodrigosuelli <rodrigosuelli@gmail.com>
2023-11-23 23:28:58 -0300

    Add layout

---
+++ src/app/(shop)/fale-conosco/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Acme - Fale Conosco',
+  description: 'Tela de fale conosco da Acme',
+};
+
+export default function FaleConoscoLayout({ children }) {
+  return children;
+}
